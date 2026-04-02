21f78fc96c321e040addb8792e9089198e6f7eda
caztillo <caztillo@gmail.com>
2026-04-02 11:48:14 -0600

    Fetch smoothie data from external API

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-
+import requests  
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -43,6 +43,9 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
+        # st.text(smoothiefroot_response.json())
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     #st.write(ingredients_string)
 
@@ -59,9 +62,6 @@ if ingredients_list:
         
         st.success('Your Smoothie is ordered, ' + name_on_order +'!', icon="✅")
 
-# New section to display smoothiefroot nutrition information
 
-import requests  
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-# st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
+
